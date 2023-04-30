c501a1da8e8fedeb3e55b8c0174593d6e3b6fc8c
Sergio Ayala T <sergioayalatapia@gmail.com>
2023-04-30 19:58:59 -0400

    apply lint to fix formatting

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './Nav';
 import Tournaments from './Tournaments';
 import Home from './Home';
 // import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; //Switch is not used in V.6
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //Switch is not used in V.6
 import Players from './Players';
 import PlayerDetail from './PlayerDetail';
 
@@ -12,14 +12,14 @@ function App() {
   return (
     <Router>
       <div className="App" style={{ backgroundImage: `url(${background})` }}>
-        <Nav/>
+        <Nav />
         {/* Route should be wrapped inside <Routes>, use element instead of component, use full component name <Tournaments/>  */}
-        <Routes> 
-          <Route path = '/' element={<Home/>}/>
-          <Route path = '/tournaments' element={<Tournaments/>}/>
-          <Route path = '/players' element={<Players/>}/>
-          <Route path = '/players/:id' element={<PlayerDetail/>}/>
-          <Route path="*" element={<Players/>} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/tournaments' element={<Tournaments />} />
+          <Route path='/players' element={<Players />} />
+          <Route path='/players/:id' element={<PlayerDetail />} />
+          <Route path="*" element={<Players />} />
         </Routes>
       </div>
     </Router>
--- src/PlayerDetail.js
+++ src/PlayerDetail.js
@@ -1,34 +1,34 @@
 import './App.css';
-import {useParams} from 'react-router-dom'; //V.6 {match} prop not supported
-import { useEffect,useState } from 'react';
+import { useParams } from 'react-router-dom'; //V.6 {match} prop not supported
+import { useEffect, useState } from 'react';
 
-const PlayerDetail = () =>{
-    let {id} = useParams();
+const PlayerDetail = () => {
+    let { id } = useParams();
     // console.log(id);
-    const [data,setData] = useState({});
+    const [data, setData] = useState({});
 
     const URL = `https://tennis-live-data.p.rapidapi.com/player/${id}`;
     const CONFIG = {
         "method": "GET",
-	    "headers": {
+        "headers": {
             "x-rapidapi-host": "tennis-live-data.p.rapidapi.com",
-    		"x-rapidapi-key": process.env.REACT_APP_KEY
+            "x-rapidapi-key": process.env.REACT_APP_KEY
         }
     };
     const getPlayerData = async () => {
-        const res = await fetch (URL,CONFIG);
+        const res = await fetch(URL, CONFIG);
         const data = await res.json();
         // console.log(data);
         setData(data.results.player)
     }
-    useEffect(() =>{
+    useEffect(() => {
         getPlayerData();
-    },[]);
-    
-    return(
+    }, []);
+
+    return (
         <div>
             {
-                
+
                 <div className='whiteText'>
                     <h1>{data.full_name}</h1>
                     <h2>Country: {data.country}</h2>
@@ -37,11 +37,11 @@ const PlayerDetail = () =>{
                     <h2>Ranking movement: {data.ranking_movement}</h2>
                     <h2>Race movement: {data.race_movement}</h2>
                 </div>
-                
-            }            
+
+            }
         </div>
     )
-    
+
 }
 
 export default PlayerDetail;
